7f2c2d3ace0fafccf66160e1993e62de283db799
Payman Sattari <psattari@gmail.com>
2024-12-07 19:43:05 +0100

    Added a layout.tsx to article for FB metadata.

--- src/app/articles/layout.tsx
+++ src/app/articles/layout.tsx
@@ -1,57 +1,17 @@
 // src/app/articles/layout.tsx
 
 import { Metadata } from 'next'
-import { siteConfig } from '@/config/metadata'
-import { articles } from '@/data/articles'
 
-type Props = {
-  params: {
-    slug: string[]
-  }
-}
-
-export async function generateMetadata(
-  props: Props
-): Promise<Metadata> {
-  // Get the current path
-  const currentPath = props.params.slug?.join('/') || '';
-  
-  // Find the article that matches this path
-  const article = articles.find(article => 
-    article.slug.replace(/^\/articles\//, '') === currentPath
-  );
-
-  if (!article) {
-    return {}; // Return empty metadata if no article found
-  }
-
+export async function generateMetadata(): Promise<Metadata> {
+  // For articles layout, we'll just ensure og:type is 'article'
+  // The specific article metadata will be handled by the page components
   return {
-    title: article.subtitle 
-      ? `${article.title}: ${article.subtitle} | ${siteConfig.name}`
-      : `${article.title} | ${siteConfig.name}`,
-    description: article.description,
     openGraph: {
-      type: 'article',
-      title: article.subtitle 
-        ? `${article.title}: ${article.subtitle}`
-        : article.title,
-      description: article.description,
-      url: `${siteConfig.url}${article.slug}`,
-      siteName: siteConfig.name,
-      images: [{
-        url: `${siteConfig.url}/images/og-image.jpg`,
-        width: 1200,
-        height: 630,
-        alt: article.title,
-      }],
-      authors: ['Payman Sattari'],
-      publishedTime: article.publishDate,
-      section: article.category,
-      tags: article.door ? [article.door] : [],
+      type: 'article'
     },
     facebook: {
       appId: '1100997351655107'
-    },
+    }
   }
 }
 
